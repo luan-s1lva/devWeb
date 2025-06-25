3d3bc1603811381c540d8af57dd24875c64f92d9
JQSPx <joao.quintilianosouza@gmail.com>
2025-06-24 22:18:42 -0300

    Header pronto

--- premium_motorsport/src/main.jsx
+++ premium_motorsport/src/main.jsx
@@ -1,10 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import Header from './componentes/Header'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Header />
   </StrictMode>,
 )
